1fdefe54ba01904bf5ffb3f17a08edeb25b9ce99
Izabela <104799350+winternightsz@users.noreply.github.com>
2024-09-05 00:32:46 +0000

    mudancas sidebar

--- src/app/page.js
+++ src/app/page.js
@@ -8,12 +8,16 @@ export default function Home() {
   const isNome = "sim";
   
   return (
-    <main className="flex flex-col bg-white items-center justify-between">
-      <Sidebar />
-      <div className="p-4 md:pt-8"><HomeBotoesGrid /></div>
-      <div className="p-4 md:pt-0"><HomeBotoesGrid isNomes={isNome} /></div>
+    <main className="flex flex-col bg-white sm:flex-row">
       
+      <aside id="default-sidebar" class="hidden sm:block sm:static sm:z-40 sm:w-64 sm:h-screen" aria-label="Sidebar">
+        <Sidebar HomeBotoesGrid isNomes={isNome} />
+      </aside>
+      <div className="sm:p-9 flex flex-col items-center">
+      {/* <div className="md:p-4 md:pt-8"><HomeBotoesGrid /></div>
+      <div className="md:p-4 md:pt-0"><HomeBotoesGrid isNomes={isNome} /></div>  */}
       <AccsGrid />
+      </div>
     </main>
   );
 }
