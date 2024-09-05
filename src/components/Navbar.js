4834fd073cd2cb4c62fced0698b534ee440ad5ca
Izabela <104799350+winternightsz@users.noreply.github.com>
2024-09-05 01:12:15 +0000

    navbar add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import NavLink from './NavLink';
 import { useState } from 'react';
 
 const Navbar = () => {
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -12,6 +13,7 @@ const Navbar = () => {
   };
 
     return (
+      <>
 <nav className="bg-white">
   <div className="max-w-screen-xl lg:rounded-lg lg:mt-2 flex bg-twOfficial items-center justify-between md:justify-evenly mx-auto p-4">
   
@@ -33,28 +35,25 @@ const Navbar = () => {
       <NavLink href="/galeria">Sobre</NavLink>
     </nav>    
 
-    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10  text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+    <button onClick={toggleMenu} type="button" className="inline-flex items-center p-2 w-10 h-10  text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" >
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
     </button>
     
+    </div>  
+</nav>
     {isOpen && (
-          <div className="absolute top-full left-0 right-0 bg-azulPrincipal text-white md:hidden">
-            <nav className="flex flex-col space-y-4 p-4">
+          <div className="top-full left-0 right-0 bg-twOfficial text-white md:hidden">
+            <div className="flex flex-col space-y-4 p-4">
               <NavLink href="/">Home</NavLink>
-              <NavLink href="/repertorio">Repertório</NavLink>
-              <NavLink href="/referencias">Referências</NavLink>
-              <NavLink href="/galeria">Galeria</NavLink>
-              <NavLink href="/quem-somos">Quem Somos</NavLink>
-              <NavLink href="/contato">Contato</NavLink>
-            </nav>
+              <NavLink href="#">Sobre</NavLink>
+            </div>
           </div>
         )}
 
-  </div>
-</nav>
+    </>
     );
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,7 +8,7 @@ export default function Sidebar ({isNomes}) {
     <>     
    
             <aside  id="separator-sidebar" className="sm:z-40 sm:w-64 hidden sm:block sm:h-screen sm:transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-                <div className="h-full px-3 py-4 sm:overflow-y-auto bg-gray-50 dark:bg-gray-800">
+                <div className="h-full px-3 py-4 sm:overflow-y-auto ">
                     <ul className="space-y-2 font-medium">
                         <li className="grid md:grid-flow-row gap-2 p-2 lg:p-4">
                             {BotoesTipo.map((card, index) => (
